9798364f56d15c0c70e2c20cbe8023d0c0e89255
PublicCode1337 <admin@durchlasten.world>
2023-12-03 18:21:27 +0100

    new font

--- src/main/java/me/deepcraft/DeepCraft.java
+++ src/main/java/me/deepcraft/DeepCraft.java
@@ -5,8 +5,8 @@ import me.deepcraft.commands.CommandManager;
 
 public class DeepCraft {
     public static String name = "DeepCraft";
-    public static String version = "b1.0";
-    public static String prefix = "§4§lDeep§c§lCraft§7» ";
+    public static String version = "b1.5";
+    public static String prefix = "§5§lDeep§d§lCraft§7» ";
     public static CommandManager commandManager = new CommandManager();
     public static void start() {
 
