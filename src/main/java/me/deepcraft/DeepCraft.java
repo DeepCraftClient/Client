aa18d321178d17e98f531ef3e034bc2b8793e8fe
gamedevcode <darioszczepanski0308@gmail.com>
2023-12-03 11:32:18 +0100

    g

--- src/main/java/me/deepcraft/DeepCraft.java
+++ src/main/java/me/deepcraft/DeepCraft.java
@@ -1,11 +1,9 @@
 package me.deepcraft;
 
-import me.deepcraft.utils.ChatUtil;
-import net.minecraft.client.Minecraft;
 
 public class DeepCraft {
     public static String name = "DeepCraft";
-    public static String version = "b1";
+    public static String version = "b1.0";
     public static String prefix = "§4§lDeep§c§lCraft§7» ";
     public static void start() {
 
